52ef6296a4a4fa287b293744748800697fe3871a
stap780 <panaet80@mail.ru>
2024-01-05 19:49:48 +0300

    Update spree_related_product.rb

--- config/initializers/spree_related_product.rb
+++ config/initializers/spree_related_product.rb
@@ -3,14 +3,14 @@ Rails.application.config.after_initialize do
       Spree::Admin::MainMenu::SectionBuilder.new('RelationTypes', 'inbox-fill.svg').
          with_admin_ability_check(Spree::RelationType).
          with_items([
-           Spree::Admin::MainMenu::ItemBuilder.new('RelationTypes', Spree::Core::Engine.routes.url_helpers.admin_relation_types_path).build
+           Spree::Admin::MainMenu::ItemBuilder.new(Spree.t(:relation_types), Spree::Core::Engine.routes.url_helpers.admin_relation_types_path).build
          ]).
          build
     )
 end
 Rails.application.config.after_initialize do
   Rails.application.config.spree_backend.tabs[:product].add(
-    Spree::Admin::Tabs::TabBuilder.new('related', ->(resource) { related_admin_product_path(product) }).
+    Spree::Admin::Tabs::TabBuilder.new(Spree.t(:related_products), ->(resource) { related_admin_product_url(@product) }).
       with_icon_key('view.svg').
       with_active_check.
       build
